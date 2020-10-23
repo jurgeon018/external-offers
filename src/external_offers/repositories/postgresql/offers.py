b80e52fdc53a4b170ba124d69bbe489795cd0f7b
Александр Киверин <a.kiverin@cian.ru>
2020-10-23 21:09:53 +0500

    Добавлена кнопка выдачи объявлений оператору

--- src/external_offers/repositories/postgresql/offers.py
+++ src/external_offers/repositories/postgresql/offers.py
@@ -23,3 +23,40 @@ async def get_offers_in_progress_by_operator(operator_id: int) -> List[Offer]:
     rows = await pg.get().fetch(query, operator_id)
 
     return [offer_mapper.map_from(row) for row in rows]
+
+
+async def set_waiting_offers_in_progress_by_client(client_id: int) -> None:
+    query = """
+        UPDATE
+            offers_for_call
+        SET
+            status='inProgress'
+        WHERE
+            status = 'waiting'
+            AND client_id = $1;
+    """
+
+    await pg.get().execute(query, client_id)
+
+
+async def exists_offers_in_progress_by_operator(operator_id: int) -> bool:
+    query = """
+        SELECT
+            EXISTS(
+                SELECT
+                    ofc.*
+                FROM
+                    offers_for_call as ofc
+                INNER JOIN
+                    clients as c
+                ON
+                    ofc.client_id = c.client_id
+                WHERE
+                    status = 'inProgress'
+                    AND c.operator_user_id = $1
+            )
+    """
+
+    exists = await pg.get().fetchval(query, operator_id)
+
+    return exists
--- src/external_offers/web/handlers/admin.py
+++ src/external_offers/web/handlers/admin.py
@@ -1,5 +1,13 @@
-from external_offers.repositories.postgresql import get_client_by_operator, get_offers_in_progress_by_operator
-from external_offers.templates import get_external_offers_html
+import json
+
+from external_offers.repositories.postgresql import (
+    assign_waiting_client_to_operator,
+    exists_offers_in_progress_by_operator,
+    get_client_by_operator,
+    get_offers_in_progress_by_operator,
+    set_waiting_offers_in_progress_by_client,
+)
+from external_offers.templates import get_offers_list_html
 from external_offers.web.handlers.base import PublicHandler
 
 
@@ -7,12 +15,39 @@ class AdminOffersListPageHandler(PublicHandler):
     # pylint: disable=abstract-method
 
     async def get(self) -> None:
+        self.set_header('Content-Type', 'text/html')
+
         client = await get_client_by_operator(self.realty_user_id)
         offers = await get_offers_in_progress_by_operator(self.realty_user_id)
 
-        self.set_header('content-type', 'text/html')
-        self.write(get_external_offers_html(
+        self.write(get_offers_list_html(
                 offers=offers,
                 client=client
             )
         )
+
+
+class AdminUpdateOffersListPageHandler(PublicHandler):
+    # pylint: disable=abstract-method
+
+    async def post(self) -> None:
+        self.set_header('Content-Type', 'application/json')
+
+        exists = await exists_offers_in_progress_by_operator(self.realty_user_id)
+
+        if exists:
+            self.write(json.dumps({
+                'success': False,
+                'errors': [{
+                    'message': 'Есть объявления в работе, завершите их',
+                    'code': 'offersInProgressExist'
+                }],
+            }))
+            return
+        client_id = await assign_waiting_client_to_operator(self.realty_user_id)
+        await set_waiting_offers_in_progress_by_client(client_id)
+
+        self.write(json.dumps({
+                'success': True,
+                'errors': [],
+            }))
