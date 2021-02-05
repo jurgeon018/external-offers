381a34e617463157471686051947d3d2968ab267
Александр Киверин <a.kiverin@cian.ru>
2021-02-05 11:32:08 +0300

    Переименовал модуль

--- src/external_offers/services/prioritizers/__init__.py
+++ src/external_offers/services/prioritizers/__init__.py
@@ -1,2 +1,2 @@
-from external_offers.services.prioritizers.prioritize_homowner import prioritize_homeowner_client
+from external_offers.services.prioritizers.prioritize_homeowner import prioritize_homeowner_client
 from external_offers.services.prioritizers.prioritize_smb import prioritize_smb_client
