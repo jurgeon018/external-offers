7011e3e78f62cd54e697eaf82deae832154c766c
Александр Киверин <a.kiverin@cian.ru>
2021-06-03 16:11:10 +0300

    CD-109265 Убраны оставшиеся модели

--- src/external_offers/queue/producers.py
+++ src/external_offers/queue/producers.py
@@ -5,7 +5,7 @@ from cian_core.context import get_operation_id
 from cian_core.rabbitmq.decorators import mq_producer_v2
 
 from external_offers.helpers.schemas import get_entity_schema
-from external_offers.queue.entities import AnnouncementDeletedMessage, AnnouncementMessage, SourceModel
+from external_offers.queue.entities import AnnouncementMessage, SourceModel
 from external_offers.queue.routing_keys import ExternalOffersV1RoutingKey
 from external_offers.repositories.monolith_cian_announcementapi.entities import ObjectModel
 
@@ -14,14 +14,6 @@ async def _get_announcement_message(model: ObjectModel, source_model: SourceMode
     return AnnouncementMessage(
         source_model=source_model,
         model=model,
-        operation_id=get_operation_id(),
-        date=datetime.now(tz=pytz.UTC),
-    )
-
-
-async def _get_announcement_deleted_message(source_object_id: str):
-    return AnnouncementDeletedMessage(
-        source_object_id=source_object_id,
         operation_id=get_operation_id(),
         date=datetime.now(tz=pytz.UTC),
     )
