f9d40db439c907ee0ecc5765fbcc706581170f31
Andrew Mendela <jurgeon018@gmail.com>
2021-06-25 18:01:57 +0200

    CD-102520

--- src/external_offers/repositories/users/_repo.py
+++ src/external_offers/repositories/users/_repo.py
@@ -8,7 +8,6 @@ To re-generate, run `codegen generate-client users`
 cian-codegen version: 1.9.0
 
 """
-from cian_core.runtime_settings import runtime_settings
 from cian_http.api_client import Api
 
 from . import entities
@@ -21,7 +20,6 @@ v1_register_user_by_phone = _api.make_client(
     handle_http_exceptions=True,
     request_schema=entities.RegisterUserByPhoneRequest,
     response_schema=entities.RegisterUserByPhoneResponse,
-    default_timeout=runtime_settings.V1_REGISTER_USER_BY_PHONE_TIMEOUT,
 )
 v2_get_users_by_phone = _api.make_client(
     path='/v2/get-users-by-phone/',
@@ -29,5 +27,4 @@ v2_get_users_by_phone = _api.make_client(
     handle_http_exceptions=True,
     request_schema=entities.V2GetUsersByPhone,
     response_schema=entities.GetUsersByPhoneResponseV2,
-    default_timeout=runtime_settings.V2_GET_USERS_BY_PHONE_TIMEOUT,
 )
