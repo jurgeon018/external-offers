db008b17d1e67f7f234059f948ab121e76811724
Andrew Mendela <jurgeon018@gmail.com>
2021-07-23 15:19:57 +0200

    CD-110127 поправил тайпинги и убрал консольлог

--- src/external_offers/repositories/postgresql/clients.py
+++ src/external_offers/repositories/postgresql/clients.py
@@ -405,7 +405,7 @@ async def set_comment_by_client_id(
     *,
     client_id: str,
     comment: str,
-) -> Optional[Client]:
+) -> None:
     query, params = asyncpgsa.compile_query(
         update(
             clients
@@ -416,7 +416,6 @@ async def set_comment_by_client_id(
         )
     )
     await pg.get().execute(query, *params)
-
 
 
 async def set_client_accepted_and_no_operator_if_no_offers_in_progress(
