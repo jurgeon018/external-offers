d096c8d33f78d3ed7f3fd85383eed68cc8e52929
Andrew Mendela <jurgeon018@gmail.com>
2021-08-10 21:58:28 +0800

    CD-110125 передвинул проверку на row_version из функции в sql

--- src/external_offers/services/announcement.py
+++ src/external_offers/services/announcement.py
@@ -1,21 +1,12 @@
 from datetime import datetime
 
 from external_offers.repositories.monolith_cian_announcementapi.entities.object_model import ObjectModel
-from external_offers.repositories.postgresql.offers import (
-    get_offer_row_version_by_offer_cian_id,
-    set_offer_publication_status_by_offer_cian_id,
-)
+from external_offers.repositories.postgresql.offers import set_offer_publication_status_by_offer_cian_id
 
 
 async def process_announcement(object_model: ObjectModel, event_date: datetime) -> None:
-    row_version = object_model.row_version
-    cian_id = object_model.cian_id
-    status = object_model.status
-    offer_row_version = await get_offer_row_version_by_offer_cian_id(offer_cian_id=cian_id)
-    if row_version <= offer_row_version:
-        return
     await set_offer_publication_status_by_offer_cian_id(
-        offer_cian_id=cian_id,
-        publication_status=status.value,
-        row_version=row_version,
+        offer_cian_id=object_model.cian_id,
+        publication_status=object_model.status.value,
+        row_version=object_model.row_version,
     )
