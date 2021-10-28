3fd90e2fe2f18944b0f8c28210e611a362136efb
Andrew Mendela <jurgeon018@gmail.com>
2021-10-28 16:47:39 +0800

    CD-122494 поменял snake_case на camelCase при создании тестовых обьявлений

--- src/external_offers/mappers/parsed_object_model.py
+++ src/external_offers/mappers/parsed_object_model.py
@@ -3,7 +3,4 @@ from cian_entities import EntityMapper
 from external_offers.entities.parsed_offers import ParsedObjectModel
 
 
-parsed_object_model_mapper = EntityMapper(
-    ParsedObjectModel,
-    without_camelcase=True,
-)
+parsed_object_model_mapper = EntityMapper(ParsedObjectModel)
