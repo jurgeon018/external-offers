5c324ac3b0c00c02beab220a1f03bf20af3a1c17
Andrew Mendela <jurgeon018@gmail.com>
2022-02-15 15:51:17 +0800

    CD-129685 вывел колтрекинговый тип номера в карточке

--- tests/handlers/test_offers_list.py
+++ tests/handlers/test_offers_list.py
@@ -138,6 +138,7 @@ async def test_offers_list_page_handler(mocker, http_client, base_url):
         client_phones=['7343433'],
         status='inProgress'
     )
+    is_calltracking = False
     # client = mocker.MagicMock(value=None)
     offers = mocker.MagicMock(value=[])
     mocker.patch(
@@ -156,6 +157,10 @@ async def test_offers_list_page_handler(mocker, http_client, base_url):
         'external_offers.web.handlers.admin.get_operator_roles',
         return_value=future([]),
     )
+    mocker.patch(
+        'external_offers.web.handlers.admin.get_client_is_calltracking_by_client_id',
+        return_value=future(is_calltracking),
+    )
 
     # act
     with freezegun.freeze_time('2022-01-01'):
@@ -169,6 +174,7 @@ async def test_offers_list_page_handler(mocker, http_client, base_url):
 
     # assert
     get_offers_list_html_mock.assert_called_once_with(
+        client_is_calltracking=is_calltracking,
         offers=offers,
         client=client,
         client_phone=client.client_phones[0],
@@ -180,3 +186,16 @@ async def test_offers_list_page_handler(mocker, http_client, base_url):
     get_operator_roles_mock.assert_called_once_with(
         operator_id=int(user_id),
     )
+
+
+# E   Expected: 
+# client_phone='7343433',default_next_call_datetime=FakeDatetime(2022, 1, 2, 10, 0), is_calltracking=False, is_commercial_moderator=False, offers=<MagicMock id='140563414109200'>, operator_id=1, operator_is_tester=False)
+# E   Actual: 
+# client_is_calltracking=False, client_phone='7343433', default_next_call_datetime=FakeDatetime(2022, 1, 2, 10, 0), is_commercial_moderator=False, offers=<MagicMock id='140563414109200'>, operator_id=1, operator_is_tester=False)
+
+
+# E   AssertionError: expected call not found.
+# E   Expected:
+# client_phone='7343433', default_next_call_datetime=FakeDatetime(2022, 1, 2, 10, 0), is_calltracking=False, is_commercial_moderator=False, offers=<MagicMock id='140466698430640'>, operator_id=1, operator_is_tester=False)
+# E   Actual:
+# client_is_calltracking=False, client_phone='7343433', default_next_call_datetime=FakeDatetime(2022, 1, 2, 10, 0), is_commercial_moderator=False, offers=<MagicMock id='140466698430640'>, operator_id=1, operator_is_tester=False)
