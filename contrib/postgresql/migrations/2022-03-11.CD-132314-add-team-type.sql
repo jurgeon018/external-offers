278ce3b49cb706a2931c44dcd6e2b3005ca52ef0
Andrew Mendela <jurgeon018@gmail.com>
2022-03-14 18:15:21 +0800

    CD-132314 added null constraint to teams.team_type

--- contrib/postgresql/migrations/2022-03-11.CD-132314-add-team-type.sql
+++ contrib/postgresql/migrations/2022-03-11.CD-132314-add-team-type.sql
@@ -8,4 +8,4 @@ ALTER TABLE clients ADD COLUMN real_name VARCHAR;
 ALTER TABLE clients ADD COLUMN real_phone_hunted_at TIMESTAMP WITH TIME ZONE;
 ALTER TABLE teams ADD COLUMN team_type team_type_enum NULL;
 UPDATE teams SET team_type='attractor'; 
-ALTER TABLE teams ALTER COLUMN team_type set not null;
+ALTER TABLE teams ALTER COLUMN team_type SET NOT NULL;
