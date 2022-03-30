18e3900291e9ade7141fba76231616386e337a91
Andrew Mendela <jurgeon018@gmail.com>
2022-03-30 15:50:49 +0800

    CD-132314

--- src/external_offers/services/operator_roles.py
+++ src/external_offers/services/operator_roles.py
@@ -146,7 +146,7 @@ async def update_operators() -> Optional[str]:
     admin_role_name = runtime_settings.ADMIN_OPERATOR_ROLE
     try:
         users = await get_users_by_role(role_name=admin_role_name)
-    except ApiClientException as e:
+    except Exception as e:
         return str(e.message)
     for user in users:
         operator_id = user.id  # or user.cian_user_id
