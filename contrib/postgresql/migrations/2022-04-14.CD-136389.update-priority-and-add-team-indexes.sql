b74dec7feccd93128c1abf00a570335852957211
Andrew Mendela <jurgeon018@gmail.com>
2022-04-14 16:14:03 +0800

    CD-136389 добавил миграции с индексами на team_priorities

--- contrib/postgresql/migrations/2022-04-14.CD-136389.update-priority-and-add-team-indexes.sql
+++ contrib/postgresql/migrations/2022-04-14.CD-136389.update-priority-and-add-team-indexes.sql
@@ -1,93 +1,93 @@
 -- Обновить командный приоритет у заданий со старым приоритетом 
 update offers_for_call
-set team_priorities = jsonb_set(team_priorities, '41', '999999999999999999', true)
+set team_priorities = jsonb_set(team_priorities, '{41}', '999999999999999999', true)
 where id in (
 	select id
 	from offers_for_call
 	where team_priorities->>'41'::text = '-1'
 	limit 100000
-)
+);
 
 update offers_for_call
-set team_priorities = jsonb_set(team_priorities, '43', '999999999999999999', true)
+set team_priorities = jsonb_set(team_priorities, '{43}', '999999999999999999', true)
 where id in (
 	select id
 	from offers_for_call
 	where team_priorities->>'43'::text = '-1'
 	limit 100000
-)
+);
 
 update offers_for_call
-set team_priorities = jsonb_set(team_priorities, '57', '999999999999999999', true)
+set team_priorities = jsonb_set(team_priorities, '{57}', '999999999999999999', true)
 where id in (
 	select id
 	from offers_for_call
 	where team_priorities->>'57'::text = '-1'
 	limit 100000
-)
+);
 
 update offers_for_call
-set team_priorities = jsonb_set(team_priorities, '63', '999999999999999999', true)
+set team_priorities = jsonb_set(team_priorities, '{63}', '999999999999999999', true)
 where id in (
 	select id
 	from offers_for_call
 	where team_priorities->>'63'::text = '-1'
 	limit 100000
-)
+);
 
 update offers_for_call
-set team_priorities = jsonb_set(team_priorities, '60', '999999999999999999', true)
+set team_priorities = jsonb_set(team_priorities, '{60}', '999999999999999999', true)
 where id in (
 	select id
 	from offers_for_call
 	where team_priorities->>'60'::text = '-1'
 	limit 100000
-)
+);
 
 update offers_for_call
-set team_priorities = jsonb_set(team_priorities, '64', '999999999999999999', true)
+set team_priorities = jsonb_set(team_priorities, '{64}', '999999999999999999', true)
 where id in (
 	select id
 	from offers_for_call
 	where team_priorities->>'64'::text = '-1'
 	limit 100000
-)
+);
 
 update offers_for_call
-set team_priorities = jsonb_set(team_priorities, '62', '999999999999999999', true)
+set team_priorities = jsonb_set(team_priorities, '{62}', '999999999999999999', true)
 where id in (
 	select id
 	from offers_for_call
 	where team_priorities->>'62'::text = '-1'
 	limit 100000
-)
+);
 
 update offers_for_call
-set team_priorities = jsonb_set(team_priorities, '65', '999999999999999999', true)
+set team_priorities = jsonb_set(team_priorities, '{65}', '999999999999999999', true)
 where id in (
 	select id
 	from offers_for_call
 	where team_priorities->>'65'::text = '-1'
 	limit 100000
-)
+);
 
 update offers_for_call
-set team_priorities = jsonb_set(team_priorities, '50', '999999999999999999', true)
+set team_priorities = jsonb_set(team_priorities, '{50}', '999999999999999999', true)
 where id in (
 	select id
 	from offers_for_call
 	where team_priorities->>'50'::text = '-1'
 	limit 100000
-)
+);
 
 update offers_for_call
-set team_priorities = jsonb_set(team_priorities, '42', '999999999999999999', true)
+set team_priorities = jsonb_set(team_priorities, '{42}', '999999999999999999', true)
 where id in (
 	select id
 	from offers_for_call
 	where team_priorities->>'42'::text = '-1'
 	limit 100000
-)
+);
 
 -- Добавить индексы на командные приоритеты для быстрой выдачи задания
 create index offers_for_call_team_priority_41_created_at_idx on offers_for_call ((team_priorities -> '41'), created_at DESC);
