7eeeefdbd50ed938f51311da8b47a4b3ccf8636f
Andrew Mendela <jurgeon018@gmail.com>
2022-04-28 15:44:22 +0800

    CD-136175 removed deprecated producer

--- src/external_offers/queue/producers.py
+++ src/external_offers/queue/producers.py
@@ -4,13 +4,14 @@ from typing import Optional
 import pytz
 from cian_core.context import get_or_create_operation_id
 from cian_core.rabbitmq.decorators import mq_producer_v2
+from cian_core.rabbitmq import RabbitMQEntityProducer
 
 from external_offers.entities.operators import EnrichedOperator
 from external_offers.queue.entities import OperatorMessage
 from external_offers.queue.routing_keys import OperatorV1RoutingKey
 
 
-async def _get_operator_message(operator: Optional[EnrichedOperator]) -> OperatorMessage:
+def build_operator_message(operator: Optional[EnrichedOperator]) -> OperatorMessage:
     return OperatorMessage(
         operator=operator,
         operation_id=get_or_create_operation_id(),
@@ -18,7 +19,7 @@ async def _get_operator_message(operator: Optional[EnrichedOperator]) -> Operato
     )
 
 
-operator_producer = mq_producer_v2(
-    schema=OperatorMessage,
-    routing_key=OperatorV1RoutingKey.updated.value,
-)(_get_operator_message)
+operator_producer = RabbitMQEntityProducer(
+    OperatorMessage,
+    OperatorV1RoutingKey.updated.value,
+)
--- src/external_offers/services/operators.py
+++ src/external_offers/services/operators.py
@@ -8,7 +8,7 @@ from external_offers.entities.operators import (
     UpdateOperatorsRequest,
 )
 from external_offers.entities.response import BasicResponse
-from external_offers.queue.producers import operator_producer
+from external_offers.queue.producers import operator_producer, build_operator_message
 from external_offers.repositories.postgresql.operators import (
     create_operator,
     delete_operator_by_id,
@@ -65,7 +65,7 @@ async def update_operator_public(request: UpdateOperatorRequest, user_id: int) -
         message = 'Информация про оператора была успешно обновлена.'
         success = True
         operator = await get_enriched_operator_by_id(request.operator_id)
-        await operator_producer(operator)
+        await operator_producer(build_operator_message(operator))
     except PostgresError as e:
         message = f'Во время обновления оператора произошла ошибка: {e}'
     return BasicResponse(
